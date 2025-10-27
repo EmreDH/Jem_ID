9275f65adf9e478c19a13cb9ffb56365b0666d02
Anaselm2003 <anaslmaomc2012@gmail.com>
2025-10-27 10:20:23 +0100

    added imports in the pages

---
+++ FrontEnd/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Navbar() {
+    return (
+        <div className="App">
+            <h1>Navbar</h1>
+        </div>
+    );
+}
+
+export default Navbar;
